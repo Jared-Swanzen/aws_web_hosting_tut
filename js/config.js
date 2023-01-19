417fe48966b0e7949ecd4be291c44bd244da3b57
Jared-Swanzen <jared@bbd.co.za>
2023-01-19 11:47:43 +0200

    New config

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_LB8K8OSbY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1hrup0kpp47mjrse35963r4qjt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
